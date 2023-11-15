c2682cafed458cf5c6d66409f24fe760bb67adf5
LGPolytech <112261621+LGPolytech@users.noreply.github.com>
2023-11-15 09:21:30 +0100

    Commit 15/11

---
+++ Code/test.py
@@ -0,0 +1,17 @@
+from pytube import YouTube
+from playsound import playsound
+
+# Define the URL of the YouTube video
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"  # replace with your YouTube video URL
+
+# Create a YouTube object
+youtube = YouTube(url)
+
+# Get the highest quality audio stream
+audio_stream = youtube.streams.get_audio_only()
+
+# Download the audio stream
+filename = audio_stream.download()
+
+# Play the audio file
+playsound(filename)
