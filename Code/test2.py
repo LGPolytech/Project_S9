d67b1b1f6473ed49552f3d5b1cb8109b1ffe5d1f
LGPolytech <112261621+LGPolytech@users.noreply.github.com>
2023-12-22 15:15:16 +0100

    update 2.9

--- Code/test2.py
+++
@@ -1,8 +0,0 @@
-from youtube_dl import YoutubeDL
-
-# Create a YoutubeDL object - verbose to get more information while downloading
-
-ydl = YoutubeDL({'verbose': True})
-
-# Download video
-ydl.download(['https://www.youtube.com/watch?v=dQw4w9WgXcQ'])  # replace with your YouTube video URL
